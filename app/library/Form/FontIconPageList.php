82052edd2d26432425d02c0a7334726e9e210d80
Responsive Menu <support@responsive.menu>
2016-06-02 20:35:47 +0100

    Added ID to each Option holder in admin area for precise styling

--- app/library/Form/FontIconPageList.php
+++ app/library/Form/FontIconPageList.php
@@ -8,29 +8,23 @@ class FontIconPageList
 
 	public function render(Option $option)
 	{
-    var_dump($option);
-		echo "
-    <input
-        type='text'
-				id='{$option->getName()}'
-				name='menu[{$option->getName()}][id][]'
-				value='{$option->getValue()}' />
-    <input
-        type='text'
-				id='{$option->getName()}'
-				name='menu[{$option->getName()}][icon][]'
-				value='{$option->getValue()}' />
-    <input
-        type='text'
-				id='{$option->getName()}'
-				name='menu[{$option->getName()}][id][]'
-				value='{$option->getValue()}' />
-    <input
-        type='text'
-				id='{$option->getName()}'
-				name='menu[{$option->getName()}][icon][]'
-				value='{$option->getValue()}' />
-        ";
+    $decoded = json_decode($option);
+
+    foreach(array_combine($decoded->id, $decoded->icon) as $id => $icon):
+  		echo "
+      <input
+          type='text'
+  				id='{$option->getName()}'
+  				name='menu[{$option->getName()}][id][]'
+  				value='{$id}' />
+      <input
+          type='text'
+  				id='{$option->getName()}'
+  				name='menu[{$option->getName()}][icon][]'
+  				value='{$icon}' />
+          ";
+      endforeach;
+
 	}
 
 }
