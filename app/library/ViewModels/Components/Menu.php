8ee5e6edc8c049d3224adf833c057a37ce41c0a3
Responsive Menu <support@responsive.menu>
2016-06-07 19:27:56 +0100

    Fixed issue with Menu View Component

--- app/library/ViewModels/Components/Menu.php
+++ app/library/ViewModels/Components/Menu.php
@@ -7,16 +7,15 @@ use ResponsiveMenu\Walkers\WpWalker as Walker;
 class Menu implements ViewComponent {
 
   public function render(OptionsCollection $options) {
-
     return wp_nav_menu(
       [
         'container' => '',
         'menu_id' => 'responsive-menu',
         'menu_class' => null,
-        'menu' => isset($options['menu_name']) ? $options['menu_name'] : null,
-        'depth' => isset($options['depth']) ? $options['depth'] : 0,
-        'theme_location' => isset($options['theme_location']) ? $options['theme_location'] : null,
-        'walker' => isset($options['walker']) ? new $options['walker']($options) : new Walker($options),
+        'menu' => $options['menu_to_use']->getValue() ? $options['menu_to_use']->getValue() : null,
+        'depth' => $options['menu_depth']->getValue() ? $options['menu_depth']->getValue() : 0,
+        'theme_location' => $options['theme_location_menu']->getValue() ? $options['theme_location_menu']->getValue() : null,
+        'walker' => $options['custom_walker']->getValue() ? new $options['custom_walker']($options) : new Walker($options),
         'echo' => false
       ]
     );
