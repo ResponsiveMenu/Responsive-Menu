63878d19f801277438fc26412d3b8d03da0d5382
Responsive Menu <support@responsive.menu>
2016-07-03 22:59:52 +0100

    Enqueue jquery as its required

--- src/app/Factories/FrontDisplayFactory.php
+++ src/app/Factories/FrontDisplayFactory.php
@@ -20,6 +20,8 @@ class FrontDisplayFactory {
       return $classes;
     });
 
+    wp_enqueue_script('jquery');
+    
     if($options['external_files'] == 'on') :
       $data_folder_dir = plugins_url(). '/responsive-menu-data';
       $css_file = $data_folder_dir . '/css/responsive-menu-' . get_current_blog_id() . '.css';
