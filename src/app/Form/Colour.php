8f5beffd74d9eaf743160f731490d6f70a4cde36
Responsive Menu <support@responsive.menu>
2016-08-18 11:29:52 +0100

    Added Test for Colour Form Component

--- src/app/Form/Colour.php
+++ src/app/Form/Colour.php
@@ -1,19 +1,13 @@
 <?php
 
 namespace ResponsiveMenu\Form;
-use ResponsiveMenu\Models\Option as Option;
-use ResponsiveMenu\Form\FormComponent as FormComponent;
+use ResponsiveMenu\Models\Option;
+use ResponsiveMenu\Form\FormComponent;
 
-class Colour implements FormComponent
-{
+class Colour implements FormComponent {
 
-	public function render(Option $option)
-	{
-		echo "<input type='text'
-				class='colour wp-color-picker'
-				id='{$option->getName()}'
-				name='menu[{$option->getName()}]'
-				value='{$option->getValue()}' />";
+	public function render(Option $option) {
+		return "<input type='text' class='colour wp-color-picker' id='{$option->getName()}' name='menu[{$option->getName()}]' value='{$option->getValue()}' />";
 	}
 
 }
---
+++ tests/app/Form/ColourTest.php
@@ -0,0 +1,16 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class ColourTest extends TestCase {
+
+  public function setUp() {
+    $this->colour_form_component = new ResponsiveMenu\Form\Colour;
+  }
+
+  public function testRendering() {
+    $output = $this->colour_form_component->render(new ResponsiveMenu\Models\Option('a', 1));
+    $this->assertEquals("<input type='text' class='colour wp-color-picker' id='a' name='menu[a]' value='1' />", $output);
+  }
+
+}
