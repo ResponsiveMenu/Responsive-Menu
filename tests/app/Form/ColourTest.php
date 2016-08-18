616858a90ad9ce984812b3ab89cfaa04bcff85d9
Responsive Menu <support@responsive.menu>
2016-08-18 11:34:20 +0100

    Made colour test component variable name more generic

--- tests/app/Form/ColourTest.php
+++ tests/app/Form/ColourTest.php
@@ -5,11 +5,11 @@ use PHPUnit\Framework\TestCase;
 class ColourTest extends TestCase {
 
   public function setUp() {
-    $this->colour_form_component = new ResponsiveMenu\Form\Colour;
+    $this->form_component = new ResponsiveMenu\Form\Colour;
   }
 
   public function testRendering() {
-    $output = $this->colour_form_component->render(new ResponsiveMenu\Models\Option('a', 1));
+    $output = $this->form_component->render(new ResponsiveMenu\Models\Option('a', 1));
     $this->assertEquals("<input type='text' class='colour wp-color-picker' id='a' name='menu[a]' value='1' />", $output);
   }
 
