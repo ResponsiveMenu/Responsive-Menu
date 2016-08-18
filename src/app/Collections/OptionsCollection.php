c72c6bbbacd20980a7fa523c1ac3391c0f85d0c8
Responsive Menu <support@responsive.menu>
2016-08-18 10:02:58 +0100

    Added Options Collection test and small refactoring

--- src/app/Collections/OptionsCollection.php
+++ src/app/Collections/OptionsCollection.php
@@ -56,7 +56,7 @@ class OptionsCollection implements \ArrayAccess {
 
   public function getButtonIconActive() {
     if($this->options['button_image'] && $this->options['button_image']->getValue())
-      return '<img src="' . $this->options['button_image_when_clicked'] .'" class=" responsive-menu-button-icon responsive-menu-button-icon-inactive" />';
+      return '<img src="' . $this->options['button_image_when_clicked'] .'" class="responsive-menu-button-icon responsive-menu-button-icon-inactive" />';
   }
 
   public function offsetExists($offset) {
@@ -68,8 +68,7 @@ class OptionsCollection implements \ArrayAccess {
   }
 
   public function offsetSet($offset, $value) {
-    if(isset($this->options[$offset]))
-      $this->options[$offset]->setValue($value);
+      $this->options[$offset] = $value;
   }
 
   public function offsetUnset($offset) {
