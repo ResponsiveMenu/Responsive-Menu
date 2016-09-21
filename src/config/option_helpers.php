6eb194d4a7331168c704cf0defbf687060d09184
Responsive Menu <support@responsive.menu>
2016-09-21 23:25:59 +0100

    Added html filter to menu title

--- src/config/option_helpers.php
+++ src/config/option_helpers.php
@@ -4,6 +4,9 @@ $option_helpers = [
 	'menu_font_icons' => [
     'filter' => 'ResponsiveMenu\Filters\JsonFilter'
 	],
+  'menu_title' => [
+    'filter' => 'ResponsiveMenu\Filters\HtmlFilter'
+  ],
 	'menu_additional_content' => [
     'filter' => 'ResponsiveMenu\Filters\HtmlFilter'
 	],
