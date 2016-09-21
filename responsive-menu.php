aba1cacecb2b3fed542ee7a0d341504299faa8f4
Responsive Menu <support@responsive.menu>
2016-09-21 19:11:54 +0100

    Updated version number

--- responsive-menu.php
+++ responsive-menu.php
@@ -4,7 +4,7 @@
 Plugin Name: Responsive Menu
 Plugin URI: https://responsive.menu
 Description: Highly Customisable Responsive Menu Plugin for WordPress
-Version: 3.0.11
+Version: 3.0.12
 Author: Responsive Menu
 Text Domain: responsive-menu
 Author URI: https://responsive.menu
