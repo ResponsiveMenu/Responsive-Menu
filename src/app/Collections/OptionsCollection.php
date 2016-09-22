14a9d0d9f70eab620c931466446983c6bc6a83ee
Responsive Menu <support@responsive.menu>
2016-09-22 09:43:29 +0100

    Added correct alt text variables

--- src/app/Collections/OptionsCollection.php
+++ src/app/Collections/OptionsCollection.php
@@ -25,7 +25,7 @@ class OptionsCollection implements \ArrayAccess {
 
   public function getActiveArrow() {
     if($this->options['active_arrow_image'] && $this->options['active_arrow_image']->getValue())
-      return '<img alt="" src="' . $this->options['active_arrow_image'] .'" />';
+      return '<img alt="' . $this->options['active_arrow_image_alt'] .'" src="' . $this->options['active_arrow_image'] .'" />';
     else
       return $this->options['active_arrow_shape'];
 
@@ -33,7 +33,7 @@ class OptionsCollection implements \ArrayAccess {
 
   public function getInActiveArrow() {
     if($this->options['inactive_arrow_image'] && $this->options['inactive_arrow_image']->getValue())
-      return '<img alt="" src="' . $this->options['inactive_arrow_image'] .'" />';
+      return '<img alt="' . $this->options['inactive_arrow_image_alt'] .'" src="' . $this->options['inactive_arrow_image'] .'" />';
     else
       return $this->options['inactive_arrow_shape'];
 
@@ -41,7 +41,7 @@ class OptionsCollection implements \ArrayAccess {
 
   public function getTitleImage() {
     if($this->options['menu_title_image'] && $this->options['menu_title_image']->getValue())
-      return '<img alt="" src="' . $this->options['menu_title_image'] .'" />';
+      return '<img alt="' . $this->options['menu_title_image_alt'] .'" src="' . $this->options['menu_title_image'] .'" />';
     else
       return null;
 
@@ -49,14 +49,14 @@ class OptionsCollection implements \ArrayAccess {
 
   public function getButtonIcon() {
     if($this->options['button_image'] && $this->options['button_image']->getValue())
-      return '<img alt="" src="' . $this->options['button_image'] .'" class="responsive-menu-button-icon responsive-menu-button-icon-active" />';
+      return '<img alt="' . $this->options['button_image_alt'] .'" src="' . $this->options['button_image'] .'" class="responsive-menu-button-icon responsive-menu-button-icon-active" />';
     else
       return '<span class="responsive-menu-inner"></span>';
   }
 
   public function getButtonIconActive() {
     if($this->options['button_image'] && $this->options['button_image']->getValue())
-      return '<img alt="" src="' . $this->options['button_image_when_clicked'] .'" class="responsive-menu-button-icon responsive-menu-button-icon-inactive" />';
+      return '<img alt="' . $this->options['button_image_alt_when_clicked'] .'" src="' . $this->options['button_image_when_clicked'] .'" class="responsive-menu-button-icon responsive-menu-button-icon-inactive" />';
   }
 
   public function offsetExists($offset) {
