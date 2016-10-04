f57489b6383d8f1459a3fc55785c6be24cce64d6
Responsive Menu <support@responsive.menu>
2016-10-04 15:37:16 +0100

    Updated tests

--- tests/app/Form/HeaderBarOrderingTest.php
+++ tests/app/Form/HeaderBarOrderingTest.php
@@ -9,9 +9,9 @@ class HeaderBarOrderingTest extends TestCase {
   }
 
   public function testRendering() {
-    $output = $this->form_component->render(new ResponsiveMenu\Models\Option('a', '{"search": "on", "button": "off"}'));
+    $output = $this->form_component->render(new ResponsiveMenu\Models\Option('a', '{"search": "on", "title": "off"}'));
     $this->assertContains('order-option-switch order-option-switch-on', $output);
-    $this->assertContains('value="off" name="menu[a][button]"', $output);
+    $this->assertContains('value="off" name="menu[a][title]"', $output);
     $this->assertContains('value="on" name="menu[a][search]"', $output);
   }
 
