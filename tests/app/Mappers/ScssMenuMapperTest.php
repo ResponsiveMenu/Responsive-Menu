f382230f5f8c0a0fe3320a6afbda7e997dc2e749
Responsive Menu <support@responsive.menu>
2016-10-10 23:13:38 +0100

    Updated tests

--- tests/app/Mappers/ScssMenuMapperTest.php
+++ tests/app/Mappers/ScssMenuMapperTest.php
@@ -11,6 +11,7 @@ class ScssMenuMapperTest extends TestCase {
     $this->collection->add(new ResponsiveMenu\Models\Option('page_wrapper', '#wrapper'));
     $this->collection->add(new ResponsiveMenu\Models\Option('animation_speed', 222));
     $this->collection->add(new ResponsiveMenu\Models\Option('menu_width', 333));
+    $this->collection->add(new ResponsiveMenu\Models\Option('menu_width_unit', 'px'));
     $this->collection->add(new ResponsiveMenu\Models\Option('menu_appear_from', 'left'));
     $this->collection->add(new ResponsiveMenu\Models\Option('menu_background_colour', '#ffffff'));
     $this->collection->add(new ResponsiveMenu\Models\Option('menu_text_alignment', 'right'));
@@ -20,16 +21,21 @@ class ScssMenuMapperTest extends TestCase {
     $this->collection->add(new ResponsiveMenu\Models\Option('menu_search_box_text_colour', '#ffffff'));
     $this->collection->add(new ResponsiveMenu\Models\Option('menu_search_box_placholder_colour', '#ffffff'));
     $this->collection->add(new ResponsiveMenu\Models\Option('menu_maximum_width', 444));
+    $this->collection->add(new ResponsiveMenu\Models\Option('menu_maximum_width_unit', 'px'));
     $this->collection->add(new ResponsiveMenu\Models\Option('menu_minimum_width', 555));
+    $this->collection->add(new ResponsiveMenu\Models\Option('menu_minimum_width_unit', 'px'));
     $this->collection->add(new ResponsiveMenu\Models\Option('menu_font', 'Arial'));
     $this->collection->add(new ResponsiveMenu\Models\Option('transition_speed', 777));
     $this->collection->add(new ResponsiveMenu\Models\Option('menu_title_background_colour', '#ffffff'));
     $this->collection->add(new ResponsiveMenu\Models\Option('menu_title_colour', '#ffffff'));
     $this->collection->add(new ResponsiveMenu\Models\Option('menu_title_font_size', 888));
+    $this->collection->add(new ResponsiveMenu\Models\Option('menu_title_font_size_unit', 'px'));
     $this->collection->add(new ResponsiveMenu\Models\Option('menu_title_hover_colour', '#ffffff'));
     $this->collection->add(new ResponsiveMenu\Models\Option('menu_title_background_hover_colour', '#ffffff'));
     $this->collection->add(new ResponsiveMenu\Models\Option('menu_font_size', 999));
+    $this->collection->add(new ResponsiveMenu\Models\Option('menu_font_size_unit', 'px'));
     $this->collection->add(new ResponsiveMenu\Models\Option('menu_links_height', 1000));
+    $this->collection->add(new ResponsiveMenu\Models\Option('menu_links_height_unit', 'px'));
     $this->collection->add(new ResponsiveMenu\Models\Option('menu_item_border_colour', '#ffffff'));
     $this->collection->add(new ResponsiveMenu\Models\Option('menu_link_colour', '#ffffff'));
     $this->collection->add(new ResponsiveMenu\Models\Option('menu_item_background_colour', '#ffffff'));
@@ -41,7 +47,9 @@ class ScssMenuMapperTest extends TestCase {
     $this->collection->add(new ResponsiveMenu\Models\Option('menu_sub_arrow_background_hover_colour', '#ffffff'));
     $this->collection->add(new ResponsiveMenu\Models\Option('arrow_position', 'left'));
     $this->collection->add(new ResponsiveMenu\Models\Option('submenu_arrow_height', 2000));
+    $this->collection->add(new ResponsiveMenu\Models\Option('submenu_arrow_height_unit', 'px'));
     $this->collection->add(new ResponsiveMenu\Models\Option('submenu_arrow_width', 3000));
+    $this->collection->add(new ResponsiveMenu\Models\Option('submenu_arrow_width_unit', 'px'));
     $this->collection->add(new ResponsiveMenu\Models\Option('menu_sub_arrow_shape_colour', '#ffffff'));
     $this->collection->add(new ResponsiveMenu\Models\Option('menu_sub_arrow_border_colour', '#ffffff'));
     $this->collection->add(new ResponsiveMenu\Models\Option('menu_sub_arrow_background_colour', '#ffffff'));
@@ -51,7 +59,15 @@ class ScssMenuMapperTest extends TestCase {
     $this->collection->add(new ResponsiveMenu\Models\Option('menu_current_item_background_hover_colour', '#ffffff'));
     $this->collection->add(new ResponsiveMenu\Models\Option('menu_current_link_hover_colour', '#ffffff'));
     $this->collection->add(new ResponsiveMenu\Models\Option('menu_current_item_border_hover_colour', '#ffffff'));
+    $this->collection->add(new ResponsiveMenu\Models\Option('menu_sub_arrow_background_colour_active', '#ffffff'));
+    $this->collection->add(new ResponsiveMenu\Models\Option('menu_sub_arrow_border_hover_colour_active', '#ffffff'));
+    $this->collection->add(new ResponsiveMenu\Models\Option('menu_sub_arrow_border_colour_active', '#ffffff'));
+    $this->collection->add(new ResponsiveMenu\Models\Option('menu_sub_arrow_shape_colour_active', '#ffffff'));
+    $this->collection->add(new ResponsiveMenu\Models\Option('menu_sub_arrow_shape_hover_colour_active', '#ffffff'));
+    $this->collection->add(new ResponsiveMenu\Models\Option('menu_sub_arrow_background_hover_colour_active', '#ffffff'));
     $this->collection->add(new ResponsiveMenu\Models\Option('menu_to_hide', '#tohide'));
+    $this->collection->add(new ResponsiveMenu\Models\Option('menu_border_width', 1));
+    $this->collection->add(new ResponsiveMenu\Models\Option('menu_border_width_unit', 'px'));
     $this->scss = new scssc_free;
     $this->mapper = new ResponsiveMenu\Mappers\ScssMenuMapper($this->scss);
   }
