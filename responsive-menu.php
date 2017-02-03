9c02191837f6458df49831d6cd6dba488c683938
Peter Featherstone <hello@peterfeatherstone.com>
2017-02-03 13:56:20 +0000

    Updated author name

--- responsive-menu.php
+++ responsive-menu.php
@@ -5,7 +5,7 @@ Plugin Name: Responsive Menu
 Plugin URI: https://responsive.menu
 Description: Highly Customisable Responsive Menu Plugin for WordPress
 Version: 3.0.18
-Author: Responsive Menu
+Author: Peter Featherstone
 Text Domain: responsive-menu
 Author URI: https://responsive.menu
 License: GPL2
