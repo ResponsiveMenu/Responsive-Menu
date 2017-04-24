e9a85bee15cab2bf60a6d7ec74a14a751d3ae33c
Peter Featherstone <hello@peterfeatherstone.com>
2017-04-24 21:04:06 +0100

    Improved error message

--- app/Validation/Validators/Integer.php
+++ app/Validation/Validators/Integer.php
@@ -7,12 +7,11 @@ class Integer extends BaseValidator {
     public function validate() {
         $valid_integer = false;
 
-        // First check valid Hex
         if(is_numeric($this->data))
             $valid_integer = true;
 
         if(!$valid_integer)
-            $this->error = 'Invalid integer';
+            $this->error = 'Invalid integer - it must be a whole number';
 
         return $valid_integer;
     }
