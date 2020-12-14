74cbf065ff3238e37464f4631559b4152f953dd0
Suraj Singh <suraj@expresstech.io>
2020-12-14 13:17:11 +0530

    Added roadmap admin page

---
+++ v4.0.0/templates/rmp-roadmap.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * This is template file for roadmap amdin page.
+ *
+ * @since      4.0.0
+ *
+ * @package    responsive_menu_pro
+ */
+?>  
+<div class="wrap rmp-container">
+
+    <!-- Roadmap page title -->
+    <h1 class="wp-heading-inline"> <?php esc_html_e( 'Roadmap', 'responsive-menu-pro' ); ?> </h1>
+
+    <!--- Roadmap page contents --->
+    <div class="rmp-roadmap-page" >
+        <iframe id="rmp-roadmap-iframe" src="https://next.expresstech.io/responsive-menu"></iframe>
+    </div>
+</div>
