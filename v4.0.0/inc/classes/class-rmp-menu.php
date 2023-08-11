0dc7d8e68d30005ee55ea5ec20cbdc405ed08b99
zubairraeen <zubair@expresstech.io>
2023-08-11 15:14:05 +0530

    removed ancher tag if link not added

--- v4.0.0/inc/classes/class-rmp-menu.php
+++ v4.0.0/inc/classes/class-rmp-menu.php
@@ -275,10 +275,18 @@ if ( ! class_exists( 'RMP_Menu' ) ) :
 			}
 			?>
 			<div id="rmp-menu-title-<?php echo esc_attr( $this->menu_id ); ?>" class="rmp-menu-title">
-				<a href="<?php echo esc_url( $link_href ); ?>" target="<?php echo esc_attr( $link_target ); ?>" id="rmp-menu-title-link">
-				<?php echo wp_kses( $menu_image, wp_kses_allowed_html( 'post' ) ); ?>
-				<span><?php echo esc_html( $menu_title ); ?></span>
-				</a>
+				<?php if ( ! empty( $this->options['menu_title_link'] ) ) { ?>
+					<a href="<?php echo esc_url( $link_href ); ?>" target="<?php echo esc_attr( $link_target ); ?>" class="rmp-menu-title-link" id="rmp-menu-title-link">
+				<?php }else { ?>
+					<span class="rmp-menu-title-link">
+				<?php } ?>
+						<?php echo wp_kses( $menu_image, wp_kses_allowed_html( 'post' ) ); ?>
+						<span><?php echo esc_html( $menu_title ); ?></span>
+				<?php if ( ! empty( $this->options['menu_title_link'] ) ) { ?>
+					</a>
+				<?php }else { ?>
+					</span>
+				<?php } ?>
 			</div>
 			<?php
 		}
